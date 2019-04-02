6fecd7cec2bfa19c693708a33f8c8dd6eff8d87e
Danilo Silva Rodrigues <danilo.roddrigues@gmail.com>
2019-04-01 23:57:36 -0300

    Classes OOP JavaScript

---
+++ OOP/class/class.js
@@ -0,0 +1,10 @@
+/**
+ * Declarando Classes em JavaScript
+ */
+var Pessoa = function() {};
+
+var pessoa1 = new Pessoa();
+var pessoa2 = new Pessoa();
+
+console.log(pessoa1);
+console.log(pessoa1);
