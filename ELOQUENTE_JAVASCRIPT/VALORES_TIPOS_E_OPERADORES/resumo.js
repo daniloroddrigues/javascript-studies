9db4639312ede7f937ba87aa84509b9bd2cd9cb2
Danilo Silva Rodrigues <danilo.roddrigues@gmail.com>
2019-04-02 23:06:47 -0300

    Resumo

--- ELOQUENTE_JAVASCRIPT/VALORES_TIPOS_E_OPERADORES/resumo.js
+++ ELOQUENTE_JAVASCRIPT/VALORES_TIPOS_E_OPERADORES/resumo.js
@@ -1,4 +1,4 @@
-//(1) 6 tipos de valores no JavaScript
+// (1) 6 tipos de valores no JavaScript
 // 1. numero
 // 2. numeros indefinidos
 // 3. booleanos (true false)
@@ -7,11 +7,26 @@
 // 6. objetos
 
 // (2) Numeros Inifitos
+
 // (3) NaN qualquer coisa que não seja valida
+
 // (4) strings letras maíusculas são menores que letras minúsculas
+
 // (5) boleanos curto circuito
 //      false && oioioioio // false
 //      true || oioioioio // true
+
 // (6) codições tenária
 //      true ? 1 : 2; 1
 //      false ? 1 : 2; 2
+
+// (7) coerção de tipo = converte valores 
+//      para que consiga fazer a operação
+// == coerção de tipo
+// === verifica se é do tipo e também se são iguais de fato
+
+// (8) NaN, 0 e "", são tido como true
+//   8.1 - !NaN = true
+//   8.2 - !0 = true
+//   8.3 - !"" = true
+//   8.4 - "" == false = true
