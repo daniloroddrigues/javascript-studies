4dfcacba0e066b70ed10021b5ed0c25c6632b1aa
Danilo Silva Rodrigues <danilo.roddrigues@gmail.com>
2019-04-04 21:36:40 -0300

    Adicionado comentários

--- ELOQUENTE_JAVASCRIPT/02.ESTRUTURA_DO_PROGRAMA/20.exercicios.js
+++ ELOQUENTE_JAVASCRIPT/02.ESTRUTURA_DO_PROGRAMA/20.exercicios.js
@@ -6,16 +6,20 @@ var str = "";
 var triangulo = "#";
 var counter = 0;
 
+// ex. com while
 while (counter < 8) {
   console.log((str += triangulo));
   counter++;
 }
 
+// ex. com do while
 do {
   console.log((str += triangulo));
   counter++;
 } while (counter < 8);
 
+
+// ex. com for
 for (var i = 0; i < 8; i++) {
   console.log((str += triangulo));
 }
