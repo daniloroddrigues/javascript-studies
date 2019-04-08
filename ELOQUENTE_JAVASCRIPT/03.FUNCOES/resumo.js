ba6d280e36cdc3a3c0365ccf82844d2acdb16006
Danilo Silva Rodrigues <danilo.roddrigues@gmail.com>
2019-04-08 20:54:49 -0300

    Resumo CP03

---
+++ ELOQUENTE_JAVASCRIPT/03.FUNCOES/resumo.js
@@ -0,0 +1,5 @@
+// (1) - CLOSURE - função capaz de 
+//                 referênciar uma instancia 
+//                 de uma variável local
+
+// (2) - CALL STACK - funções são chamdas para "cima do código"
